58b69270f22c0cef1e7a9a0478493fa6324cd76d
George R. NICA <grn@infinium.io>
2022-06-25 12:35:03 +0300

    convert code to async/await style

--- handler.js
+++ handler.js
@@ -8,7 +8,7 @@ const PublicGoogleSheetsParser = require('public-google-sheets-parser');
 
 AWS.config.update({ region: "eu-west-1" });
 
-const sendEmail = (events, sender, recipient) => {
+const sendEmail = async (events, sender, recipient) => {
   const message = events.join('\n');
   var params = {
     Destination: { ToAddresses: [ recipient ] },
@@ -20,17 +20,19 @@ const sendEmail = (events, sender, recipient) => {
     },
     Source: `Daily Schedule Bot <${sender}>`
   };
-  var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise()
-  sendPromise.then(params => {
+  const ses = new AWS.SES({ apiVersion: '2010-12-01' });
+  try {
+    const status = await ses.sendEmail(params).promise();
     console.log('Successfully sent email');
-    console.log(params);
-  }).catch(error => {
+    console.log(status);
+  } catch {
     console.log('Failed sending email');
     console.log(error);
-  })
+  }
+  
 }
 
-const getEvents = (spreadsheet_items) => {
+const getEvents = async (spreadsheet_items) => {
   const events = new Array();
   spreadsheet_items.slice(1).forEach(row => {
     const keys = Object.keys(row);
@@ -55,10 +57,9 @@ const weekNo = Math.ceil(( currentDate.getDay() + 1 + numberOfDays) / 7);
 
 module.exports.run = middy(async (event, context) => {
   const parser = new PublicGoogleSheetsParser(context.config.spreadsheetId, `${currentYear}W${weekNo}`);
-  parser.parse()
-    .then(items => getEvents(items))
-    .then(events => sendEmail(events, context.config.sender, context.config.recipient))
-    .catch(error => console.log(error))
+  const items = await parser.parse();
+  const events = await getEvents(items);
+  sendEmail(events, context.config.sender, context.config.recipient);
 }).use(ssm({
   cache: false,
   setToContext: true,
