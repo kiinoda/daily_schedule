38032711cdcc2819956aca267d7823a72c659f93
George R. NICA <grn@infinium.io>
2023-01-04 16:42:51 +0200

    calculate weekNo as 0-prepended string for months up to 9

--- handler.js
+++ handler.js
@@ -178,7 +178,8 @@ function getWeekNumber(d) {
   d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
   d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
   const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+  let weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+  if (weekNo <= 9) { weekNo = `0${weekNo}` }
   return [d.getUTCFullYear(), weekNo];
 }
 
